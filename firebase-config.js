072c001694089ad1783acec12f0e433b2966b626
ModhuSudhan-oos <toolgenixs@gmail.com>
2025-07-29 19:12:56 +0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,25 @@
+// firebase-config.js
+// Firebase SDK 9.x compatible
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVpddOtX1yRxslNxh8yz3SJq53eUYhkZ0",
+  authDomain: "next-gen-186aa.firebaseapp.com",
+  projectId: "next-gen-186aa",
+  storageBucket: "next-gen-186aa.firebasestorage.app",
+  messagingSenderId: "338569531164",
+  appId: "1:338569531164:web:932df077b59a0a371b34d9",
+  measurementId: "G-7GCT5KHFQ0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, auth, db, storage };
