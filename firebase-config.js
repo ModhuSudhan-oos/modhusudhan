92852324839ec53f5f33f51d5cd9d804e4d1137a
ModhuSudhan-oos <toolgenixs@gmail.com>
2025-07-30 23:00:20 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,23 +1,23 @@
-// firebase-config.js (No changes needed, but included for completeness)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-storage.js";
+import { getFunctions } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-functions.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBVpddOtX1yRxslNxh8yz3SJq53eUYhkZ0",
   authDomain: "next-gen-186aa.firebaseapp.com",
   projectId: "next-gen-186aa",
-  storageBucket: "next-gen-186aa.firebasestorage.app",
+  storageBucket: "next-gen-186aa.appspot.com",
   messagingSenderId: "338569531164",
   appId: "1:338569531164:web:932df077b59a0a371b34d9",
   measurementId: "G-7GCT5KHFQ0"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
+const functions = getFunctions(app);
 
-export { app, auth, db, storage };
+export { app, auth, db, storage, functions };
